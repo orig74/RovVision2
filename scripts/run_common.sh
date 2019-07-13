479667149ffd26cfc36acfff8313ae2415670ea8
ori <oga13@uclive.ac.nz>
2019-07-13 08:27:09 +0300

    adding scripts

---
+++ scripts/run_common.sh
@@ -0,0 +1,39 @@
+function init_docker_image {
+tmux send-keys "cd $DRONESIMLAB_PATH/dockers/python3_dev && ./run_image.sh" ENTER
+}
+
+function new_4_win {
+tmux split-window -h
+tmux select-pane -t 0
+tmux split-window -v
+tmux select-pane -t 2
+tmux split-window -v
+}
+
+
+function new_6_win {
+tmux split-window -h
+tmux select-pane -t 0
+tmux split-window -v
+tmux select-pane -t 2
+tmux split-window -v
+tmux select-pane -t 0
+tmux split-window -v
+tmux select-pane -t 3
+tmux split-window -v
+}
+
+
+
+function run { #pane number, path, script
+tmux select-pane -t $1 
+init_docker_image
+tmux send-keys "bash" ENTER
+tmux send-keys "cd $PROJECT_PATH/$2" ENTER
+tmux send-keys "/miniconda/bin/python $3" ENTER
+
+}
+
+#tmux send-keys "python drone_main.py" ENTER
+
+
--- scripts/run_sim.sh
+++ scripts/run_sim.sh
@@ -5,31 +5,10 @@ GAME_PATH=/project_files/Ocean1_packed/LinuxNoEditor/
 PROJECT_PATH=/home/host/projects/RovVision2/
 DRONESIMLAB_PATH=../../DroneSimLab/
 
+source run_common.sh
+
 tmux kill-session -t dronelab
-
-function init_docker_image {
-tmux send-keys "cd $DRONESIMLAB_PATH/dockers/python3_dev && ./run_image.sh" ENTER
-}
-
-function new_4_win {
-tmux split-window -h
-tmux select-pane -t 0
-tmux split-window -v
-tmux select-pane -t 2
-tmux split-window -v
-}
-
-function run { #pane number, path, script
-tmux select-pane -t $1 
-init_docker_image
-tmux send-keys "bash" ENTER
-tmux send-keys "cd $PROJECT_PATH/$2" ENTER
-tmux send-keys "/miniconda/bin/python $3" ENTER
-
-}
-
 tmux new-session -d -s dronelab
-#tmux send-keys "python drone_main.py" ENTER
 
 #tmux send-keys "cd ../../dockers/unreal_engine_4 && ./attach.sh" ENTER
 
@@ -45,7 +24,7 @@ sleep 1
 
 ################## onboard part
 tmux new-window 
-new_4_win
+new_6_win
 run 0 onboard controller.py
 run 1 onboard sensors_gate.py
 
