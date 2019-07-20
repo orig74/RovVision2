before fixing rotation direction

onboard/mixer.py
@@ -29,7 +29,7 @@ def mix(up_down,left_right,fwd_back,roll,pitch,yaw,pitch_copensate=0.0,roll_cope
     dcm=todcm(0,np.deg2rad(pitch_copensate),np.deg2rad(roll_copensate)).T
     v=np.array([[fwd_back,left_right,up_down]]).T
     fwd_back,left_right,up_down=(dcm @ v).flatten()
-    if abs(pitch_copensate-90)>0.01:
+    if abs(pitch_copensate-90)>0.01: ## test for singularity
 
         v=np.array([yaw,pitch,roll]) #command mat in inertial frame
         nv=np.linalg.norm(v)*10000 #multiply by large number to get eps for rotation