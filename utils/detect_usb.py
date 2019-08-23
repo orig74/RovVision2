371fe4fb4a770a309c0982b748e8d648a719d1a2
Og <o@g>
2019-08-23 14:05:16 +1200

    update

--- utils/detect_usb.py
+++ utils/detect_usb.py
@@ -2,23 +2,37 @@ import os
 import pickle
 
 if __name__=='__main__':
-    devices=os.popen('find /sys/bus/usb/devices/usb*/ -name dev |grep  tty').readlines()
     dmap = {}
-    for l in devices:
-        dname = l.strip().split('/')[-2]
-        tofind='udevadm info -q property -p %s |grep ID_SERIAL='%l.strip()[:-4]
-        lname = os.popen(tofind).read().split('=')[1].strip()
-        #print(dname,lname)
-        if '1a86' in lname:
-            dmap['ESC_USB']='/dev/'+dname
-        if 'Arduino_LLC_Arduino_Leonardo' in lname:
-            dmap['PERI_USB']='/dev/'+dname
-        if 'CP210' in lname:
-            dmap['SONAR_USB']='/dev/'+dname
-        if 'FTDI' in lname:
-            dmap['VNAV_USB']='/dev/'+dname
-    with open('/tmp/devusbmap.pkl','wb') as fp:
-        pickle.dump(dmap,fp)
+    if 0:
+        devices=os.popen('find /sys/bus/usb/devices/usb*/ -name dev |grep  tty').readlines()
+        for l in devices:
+            dname = l.strip().split('/')[-2]
+            tofind='udevadm info -q property -p %s |grep ID_SERIAL='%l.strip()[:-4]
+            lname = os.popen(tofind).read().split('=')[1].strip()
+            #print(dname,lname)
+            if '1a86' in lname:
+                dmap['ESC_USB']='/dev/'+dname
+            if 'Arduino_LLC_Arduino_Leonardo' in lname:
+                dmap['PERI_USB']='/dev/'+dname
+            if 'CP210' in lname:
+                dmap['SONAR_USB']='/dev/'+dname
+            if 'FTDI' in lname:
+                dmap['VNAV_USB']='/dev/'+dname
+    else:
+        for dev in ['/dev/ttyUSB%d'%i for i in [0,1,2]]:
+            cmd = 'udevadm info '+dev
+            #print(cmd)
+            line=os.popen(cmd).readline()
+            if '1-7' in line:
+                dmap['PERI_USB']=dev
+            if '1-5' in line:
+                dmap['ESC_USB']=dev
+            if '1-3' in line:
+                dmap['VNAV_USB']=dev
+
+        with open('/tmp/devusbmap.pkl','wb') as fp:
+            #print(dmap)
+            pickle.dump(dmap,fp)
 
 else:
     if not os.path.isfile('/tmp/devusbmap.pkl'):
