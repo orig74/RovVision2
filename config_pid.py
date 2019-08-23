ef744ceb75a62cb9ed55cba22323d11efdb6c176
uav <oga13@uclive.ac.nz>
2019-08-23 15:45:08 +1200

    successful tracking experiment

--- config_pid.py
+++ config_pid.py
@@ -12,7 +12,8 @@ depth_pid={\
         'initial_i':0,
         'func_in_err':None}
 
-ys=0.06
+ys=0.03
+#ys=0.00
 yaw_pid={\
         'P':1.5*ys,
         'I':0.000*ys,
@@ -26,8 +27,9 @@ yaw_pid={\
         'func_in_err':None}
 
 rs=0.001
+#rs=0.000
 roll_pid={\
-        'P':20*rs,
+        'P':10*rs,
         'I':0.000*rs,
         'D':2.6*rs,
         'limit':0.2,
@@ -39,10 +41,11 @@ roll_pid={\
         'func_in_err':None}
 
 ps=0.001
+#ps=0.000
 pitch_pid={\
-        'P':20*ps,
+        'P':5*ps,
         'I':0.000*ps,
-        'D':2*ps,
+        'D':5*ps,
         'limit':0.2,
         'step_limit':0.05,
         'i_limit':0.01,
@@ -54,8 +57,33 @@ pitch_pid={\
 #if set to true always try to mantain 0 roll
 roll_target_0 = True
 
-sc=0.3
-pos_pid={\
+sc=0.03
+pos_pid_x={\
+        'P':2.5 * sc ,
+        'I':0.001 * sc ,
+        'D':5 * sc ,
+        'limit':0.6,
+        'step_limit':0.05,
+        'i_limit':0.01,
+        'FF':0,
+        'angle_deg_type':True,
+        'initial_i':0,
+        'func_in_err':None}
+sc=0.09
+pos_pid_y={\
+        'P':2.5 * sc ,
+        'I':0.001 * sc ,
+        'D':5 * sc ,
+        'limit':0.6,
+        'step_limit':0.05,
+        'i_limit':0.01,
+        'FF':0,
+        'angle_deg_type':True,
+        'initial_i':0,
+        'func_in_err':None}
+
+sc=0.00
+pos_pid_z={\
         'P':2.5 * sc ,
         'I':0.001 * sc ,
         'D':5 * sc ,
@@ -68,4 +96,7 @@ pos_pid={\
         'func_in_err':None}
 
 
-pos_pids=[pos_pid]*3
+
+
+
+pos_pids=[pos_pid_x, pos_pid_y, pos_pid_z]
