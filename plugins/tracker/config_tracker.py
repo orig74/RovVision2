0902387109986c06350b642d37982f1108d408ac
ori.ganoni@pg.canterbury.ac.nz <oga13@uclive.ac.nz>
2019-08-26 03:27:10 +0300

    update tracker params

--- plugins/tracker/config_tracker.py
+++ plugins/tracker/config_tracker.py
@@ -2,7 +2,7 @@ import numpy as np
 track_offx=80
 #track_params = (60,60,60,60,track_offx,0)
 track_params = (80,80,80,80,track_offx,0)
-stereo_corr_params = {'ws':(96,96),'sxl':250+50,'sxr':0,'ofx':80}
+stereo_corr_params = {'ws':(128,128),'sxl':250+50+200,'sxr':0,'ofx':80}
 fov=60.97
 pixelwidthx = 960
 pixelwidthy = 600  
