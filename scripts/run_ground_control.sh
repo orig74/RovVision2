a5dc3f5d35e88146c9781f4b05fe3db49f042453
ori <oga13@uclive.ac.nz>
2019-09-13 11:38:23 +0300

    adding web capabilities

--- scripts/run_ground_control.sh
+++ scripts/run_ground_control.sh
@@ -19,7 +19,8 @@ fi
 new_4_win
 run 0 ground_control joy_rov.py
 run 1 ground_control viewer.py
-
+run 2 web "--version && FLASK_APP=server.py flask run"
+run 3 web "--version && sleep 3 && firefox http://127.0.0.1:5000/static/html/checklists.html --new-window"
 if [ ! -v SIM ]
 then 
 tmux att
---
+++ web/server.py
@@ -0,0 +1,17 @@
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+#FLASK_APP=server.py flask run
+from flask_socketio import SocketIO
+from flask import render_template
+from init import app,socketio
+#import commands
+
+#import pid
+
+@app.route('/')
+def index():
+    return open('static/index.html').read()
+    #return "Hello, World!"
+
+if __name__ == '__main__':
+    #app.run(host='127.0.0.1', debug=True)
+    socketio = SocketIO(app,host='0.0.0.0', debug=True)
