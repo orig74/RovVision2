ec9e2ac6e0b866d08e889aa4c7907d7a063bae5b
og <o@g>
2019-10-03 07:02:23 +1300

    vnav read directly from serial

--- hw/vnav.py
+++ hw/vnav.py
@@ -21,23 +21,43 @@ vn_setoutput_quaternion =b'$VNWRG,06,2*XX'
 #header VNYMR
 vn_setoutput=b'$VNWRG,06,14*XX' 
 ##### 
+vn_setfreq20hz=b'$VNWRG,07,20*XX'
 
 print('connecting to vnav')
 #s=EzAsyncData.connect(detect_usb.devmap['VNAV_USB'],115200)
-def init_serial():
-    import detect_usb
-    ser = serial.Serial(detect_usb.devmap['VNAV_USB'],115200)
+def write(ser,cmd):
+    ser.write(cmd+b'\n')
+    time.sleep(0.05)
+    print('>',ser.readline())
+
+def init_serial(dev=None):
+    if dev is None:
+        import detect_usb
+        dev=detect_usb.devmap['VNAV_USB']
+
+    ser = serial.Serial(dev,115200)
+    #set freq and output
+    write(ser,vn_pause)
+    write(ser,vn_setoutput)
+    write(ser,vn_setfreq20hz)
+    #set frequency
+
+
+    write(ser,vn_resume)
     return ser
 
 def parse_line(line):
-    parts=line.strip().split(b'*')[0].split(b',')
-    if parts[0]==b'$VNRRG':
-        y,p,r,mx,my,mz,ax,ay,az,gx,gy,gz = map(float,parts[2:])
-    ret={}
-    ret['ypr'] = (y,p,r)
-    ret['rates'] = (gx,gy,gz)
-    ret['mag'] = (mx,my,mz)
-    ret['acc'] = (ax,ay,az)
+    if line.startswith(b'$'):
+        parts=line.strip().split(b'*')[0].split(b',')
+        if parts[0]==b'$VNYMR':
+            y,p,r,mx,my,mz,ax,ay,az,gx,gy,gz = map(float,parts[1:])
+        ret={}
+        #ret['ypr'] = (y,p,r)
+        ret['yaw'],ret['pitch'],ret['roll']=y,p,r
+        ret['rates'] = (gx,gy,gz)
+        ret['mag'] = (mx,my,mz)
+        ret['acc'] = (ax,ay,az)
+        return ret
 
 
 print('done')
@@ -52,6 +72,25 @@ def get_data():
     ret['ypr']=(ypr.x,ypr.y,ypr.z)
     ret['rates']=(rates.x,rates.y,rates.z)
     return ret
+
+def recv_and_process2(ser):
+    cnt=0
+    while 1:
+        l=ser.readline()
+        try:
+            #import ipdb;ipdb.set_trace()
+            imu=parse_line(l)
+            tic=time.time()
+            imu['ts']=tic
+        except:
+            print('fail to parse line')
+            imu=None
+        if imu is not None:
+            if cnt%5==0:
+                print('dsim Y{:4.2f} P{:4.2f} R{:4.2f}'.format(imu['yaw'],imu['pitch'],imu['roll'])
+                            +' X{:4.2f} Y{:4.2f} Z{:4.2f}'.format(*imu['rates']))
+            pub_imu.send_multipart([zmq_topics.topic_imu,pickle.dumps(imu)])
+            cnt+=1
 
 def recv_and_process():
     cnt=0
@@ -92,7 +131,7 @@ def recv_and_process():
         time.sleep(1.0/hz)
 
 if __name__=='__main__':
-    #ser = init_serial()
-    #recv_and_process()
-    testline=b'$VNRRG,27,+006.380,+000.023,-001.953,+1.0640,-0.2531,+3.0614,+00.005,+00.344,-09.758,-0.001222,-0.000450,-0.001218*4F'
-    print(parse_line(testline))
+    ser = init_serial('/dev/ttyUSB0')
+    recv_and_process2(ser)
+    #testline=b'$VNRRG,27,+006.380,+000.023,-001.953,+1.0640,-0.2531,+3.0614,+00.005,+00.344,-09.758,-0.001222,-0.000450,-0.001218*4F'
+    #print(parse_line(testline))
