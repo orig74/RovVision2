a49bddbc6ad33d54582eb760626eea5b5180e5f5
og <o@g>
2021-04-13 12:52:23 +1200

    Calib more info + ROI

--- utils/camCalib.py
+++ utils/camCalib.py
@@ -7,6 +7,8 @@ CHKBD_DIMS = (4, 11)
 CHKBD_SCALE = 0.03
 CALIB_DIR = "/home/stereo/CalibParams/"
 
+ALPHA = 1
+
 
 class Calibrator():
     def __init__(self):
@@ -26,13 +28,6 @@ class Calibrator():
         # plt.show()
 
     def LoadCalib(self):
-        self.LeftStereoMapX = np.load(CALIB_DIR + "Left_Stereo_Map_x.npy")
-        self.LeftStereoMapY = np.load(CALIB_DIR + "Left_Stereo_Map_y.npy")
-        self.RoiL = np.load(CALIB_DIR + "RoiL.npy")
-        self.RightStereoMapX = np.load(CALIB_DIR + "Right_Stereo_Map_x.npy")
-        self.RightStereoMapY = np.load(CALIB_DIR + "Right_Stereo_Map_y.npy")
-        self.RoiR = np.load(CALIB_DIR + "RoiR.npy")
-        self.ValidCalib = True
         try:
             self.LeftStereoMapX = np.load(CALIB_DIR + "Left_Stereo_Map_x.npy")
             self.LeftStereoMapY = np.load(CALIB_DIR + "Left_Stereo_Map_y.npy")
@@ -72,24 +67,25 @@ class Calibrator():
             imgpoints2, _ = cv2.projectPoints(obj_pnts[i], rvecs[i], tvecs[i], mtx, dist)
             error = cv2.norm(img_pnts[i], imgpoints2, cv2.NORM_L2)/len(imgpoints2)
             mean_error += error
-        print("Total Error: {}".format(mean_error/len(obj_pnts)))
+        return mean_error / len(obj_pnts)
 
     def CalibMono(self, img_points, obj_points):
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, self.imshape[::-1], None, None, flags=cv2.CALIB_RATIONAL_MODEL)
-        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, self.imshape[::-1], 1, self.imshape[::-1])
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, self.imshape[::-1], ALPHA, self.imshape[::-1])
         print(roi)
-        self.CalcMonoError(img_points, obj_points, rvecs, tvecs, mtx, dist)
-        return mtx, dist, newcameramtx, roi
+        error = self.CalcMonoError(img_points, obj_points, rvecs, tvecs, mtx, dist)
+        print("Total Error: {}".format(error))
+        return mtx, dist, newcameramtx, roi, error
 
     def RunStereoCalibration(self, calIdxStep=1):
         print("Calibrating on {} imgs...".format(len(self.imgPnts_l[::calIdxStep])))
         print("Left:")
-        mtx_l, dist_l, newcameramtx_l, roi_l = self.CalibMono(self.imgPnts_l[::calIdxStep], self.objPnts[::calIdxStep])
+        mtx_l, dist_l, newcameramtx_l, roi_l, err_l = self.CalibMono(self.imgPnts_l[::calIdxStep], self.objPnts[::calIdxStep])
         print("Right:")
-        mtx_r, dist_r, newcameramtx_r, roi_r = self.CalibMono(self.imgPnts_r[::calIdxStep], self.objPnts[::calIdxStep])
+        mtx_r, dist_r, newcameramtx_r, roi_r, err_r = self.CalibMono(self.imgPnts_r[::calIdxStep], self.objPnts[::calIdxStep])
 
-        # cv2.CALIB_RATIONAL_MODEL + cv2.CALIB_FIX_INTRINSIC +
-        flags = cv2.CALIB_TILTED_MODEL + cv2.CALIB_FIX_K3 #+ cv2.CALIB_FIX_K4 + cv2.CALIB_FIX_K5 + cv2.CALIB_FIX_K6
+        # cv2.CALIB_RATIONAL_MODEL + cv2.CALIB_FIX_INTRINSIC + cv2.CALIB_TILTED_MODEL + 
+        flags = cv2.CALIB_FIX_K3 + cv2.CALIB_FIX_K4 + cv2.CALIB_FIX_K5 + cv2.CALIB_FIX_K6
         criteria_stereo = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
         retS, new_mtxL, distL, new_mtxR, distR, Rot, Trns, Emat, Fmat = cv2.stereoCalibrate(self.objPnts[::calIdxStep],
                                                                                             self.imgPnts_l[::calIdxStep],
@@ -99,7 +95,7 @@ class Calibrator():
         print("Stereo Relative Rotation: {}".format(Rot))
         print("Stereo Relative Translation: {}".format(Trns))
         rectify_scale = 1
-        rect_l, rect_r, proj_mat_l, proj_mat_r, Q, self.RoiL, self.RoiR = cv2.stereoRectify(new_mtxL, distL, new_mtxR, distR, self.imshape[::-1], Rot, Trns, rectify_scale, (0, 0))
+        rect_l, rect_r, proj_mat_l, proj_mat_r, Q, self.RoiL, self.RoiR = cv2.stereoRectify(new_mtxL, distL, new_mtxR, distR, self.imshape[::-1], Rot, Trns, rectify_scale, (0, 0), alpha=ALPHA)
         self.LeftStereoMapX, self.LeftStereoMapY = cv2.initUndistortRectifyMap(new_mtxL, distL, rect_l, proj_mat_l, self.imshape[::-1], cv2.CV_16SC2)
         self.RightStereoMapX, self.RightStereoMapY = cv2.initUndistortRectifyMap(new_mtxR, distR, rect_r, proj_mat_r, self.imshape[::-1], cv2.CV_16SC2)
 
@@ -110,19 +106,28 @@ class Calibrator():
             self.LoadCalib()
         else:
             print("Saving parameters ......")
+            with open(CALIB_DIR + "calib_output.txt", 'w') as out_file:
+                out_file.write("Error Left: {}\n".format(err_l))
+                out_file.write("Stereo ROI Left: {}\n".format(self.RoiL))
+                out_file.write("Error Right: {}\n".format(err_r))
+                out_file.write("Stereo ROI Right: {}\n".format(self.RoiR))
+            out_file.close()
+            np.save(CALIB_DIR + "Stereo_Trans", Trns)
+            np.save(CALIB_DIR + "Stereo_Rot", Rot)
+            np.save(CALIB_DIR + "Q", Q)
+            np.save(CALIB_DIR + "ProjMatLeft", proj_mat_l)
+            np.save(CALIB_DIR + "ProjMatRight", proj_mat_r)
+            np.save(CALIB_DIR + "Left_Cam_Matrix", new_mtxL)
             np.save(CALIB_DIR + "Left_Stereo_Map_x", self.LeftStereoMapX)
             np.save(CALIB_DIR + "Left_Stereo_Map_y", self.LeftStereoMapY)
+            np.save(CALIB_DIR + "RoiL", self.RoiL)
+            np.save(CALIB_DIR + "Right_Cam_Matrix", new_mtxR)
             np.save(CALIB_DIR + "Right_Stereo_Map_x", self.RightStereoMapX)
             np.save(CALIB_DIR + "Right_Stereo_Map_y", self.RightStereoMapY)
             np.save(CALIB_DIR + "RoiR", self.RoiR)
-            np.save(CALIB_DIR + "RoiL", self.RoiL)
             self.ValidCalib = True
 
     def StereoRectify(self, l_img, r_img):
         l_img_sr = cv2.remap(l_img, self.LeftStereoMapX, self.LeftStereoMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT, 0)
-        x, y, w, h = self.RoiL
-        l_img_sr_roi = l_img_sr[y:y+h, x:x+w]
         r_img_sr = cv2.remap(r_img, self.RightStereoMapX, self.RightStereoMapY, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT, 0)
-        x, y, w, h = self.RoiR
-        r_img_sr_roi = r_img_sr[y:y+h, x:x+w]
-        return l_img_sr_roi, r_img_sr_roi
+        return l_img_sr, r_img_sr
