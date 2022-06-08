85766cf99d2cdaacbd861709b373f15da28b8d07
ori <oga13@uclive.ac.nz>
2022-06-08 21:31:22 +0300

    add dvl support for sim start

--- sim/run_dynamic_sim.py
+++ sim/run_dynamic_sim.py
@@ -28,6 +28,7 @@ if savetofile:
 pub_pos_sim = utils.publisher(ue4_zmq_topics.zmq_pub_drone_fdm[1])
 pub_imu = utils.publisher(zmq_topics.topic_imu_port)
 pub_depth = utils.publisher(zmq_topics.topic_depth_port)
+pub_dvl = utils.publisher(zmq_topics.topic_dvl_port)
 
 subs_socks=[]
 subs_socks.append(utils.subscribe([zmq_topics.topic_thrusters_comand],zmq_topics.topic_thrusters_comand_port))
@@ -56,6 +57,7 @@ def get_next_state(curr_q,curr_u,control,dt,lamb):
 async def pubposition():
     curr_q = np.zeros(6)
     curr_u = np.zeros(6)
+    cnt=0
     while keep_running:
         await asyncio.sleep(dt)
         next_q,next_u=get_next_state(curr_q,curr_u,current_command,dt,lamb)
@@ -98,6 +100,21 @@ async def pubposition():
                     +' X{:4.2f} Y{:4.2f} Z{:4.2f}'.format(*curr_q[:3]),file=savefd)
         pub_imu.send_multipart([zmq_topics.topic_imu,pickle.dumps(imu)])
         pub_depth.send_multipart([zmq_topics.topic_depth,pickle.dumps({'ts':tic,'depth':curr_q[2]})])
+
+        if cnt%5==0:
+            #simulate dvl messgaes
+            vel_msg=b'wrz,%f,%f,%f,y,1.99,0.006,3.65e-05;3.39e-06;7.22e-06;3.39e-06;2.46e-06;-8.5608e-07;7.223e-06;-8.560e-07;3.2363e-06,1550139816188624,1550139816447957,188.80,0*a1\r\n'.format(*curr_u[:3])
+
+
+            pub_dvl.send_multipart([zmq_topics.topic_dvl_raw,pickle.dumps({'ts':tic,'dvl_raw':vel_msg})])
+
+        if cnt%5==1:
+            pos_msg=b'wrp,1550139816.178,%f,%f,%f,%f,2.5,-3.7,-62.5,0*3c\r\n'%(*curr_q[:3],100)
+            pub_dvl.send_multipart([zmq_topics.topic_dvl_raw,pickle.dumps({'ts':tic,'dvl_raw':pos_msg
+                })])
+
+
+        cnt+=1
 
 async def recv_and_process():
     global current_command
