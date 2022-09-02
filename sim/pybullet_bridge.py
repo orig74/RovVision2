7c8fa47543a456970de9f26f9bd794c4d19e0436
ori <oga13@uclive.ac.nz>
2022-09-02 10:41:10 +0300

    update pybullet

--- sim/pybullet_bridge.py
+++ sim/pybullet_bridge.py
@@ -20,6 +20,8 @@ import cv2
 from numpy import sin,cos
 from scipy.spatial.transform import Rotation as R
 
+from mussels_scene import getscene
+
 zmq_sub=utils.subscribe([zmq_topics.topic_thrusters_comand],zmq_topics.topic_thrusters_comand_port)
 zmq_pub=utils.publisher(zmq_topics.topic_camera_port)
 pub_imu = utils.publisher(zmq_topics.topic_imu_port)
@@ -51,23 +53,6 @@ planeId = pb.loadURDF("plane.urdf")
 pb.resetBasePositionAndOrientation(planeId,(0,0,-20),pb.getQuaternionFromEuler((0,0,0,)))
 import random
 robj=[]
-def set_random_objects():
-    random.seed(0)
-    for row in [0,1]:
-        for col in [0,1,2]:
-            for _ in range(200):
-                urdf = "random_urdfs/{0:03d}/{0:03d}.urdf".format(random.randint(1+row*50*3+col*50,50+row*50*3+col*50)) 
-                x = col*1.5 + (random.random()-0.5)*.3
-                y = row*1.5 + (random.random()-0.5)*.3
-                z = (random.random())*-10
-                try:
-                    obj = pb.loadURDF(urdf,basePosition=[x,y,z],globalScaling=3,baseOrientation=[random.random() for _ in range(4)])
-                    robj.append(obj)
-                    print('---loading--',urdf,x,y)
-                except:
-                    print('fail to load object',x,y)
-                #pb.resetBasePositionAndOrientation(obj,(x,y,z),pb.getQuaternionFromEuler((0,0,0,)))
-
 #set_random_objects()
 keep_running = True
 
@@ -111,46 +96,6 @@ def getrov():
                           basePosition=[0,0,0],
                           useMaximalCoordinates=True)
     return boxId
-
-
-def gen_rope(x,y):
-    ret=[]
-    meshScale = np.ones(3)*0.2
-    for i in range(20):
-        vfo=pb.getQuaternionFromEuler(np.deg2rad([90, 0, i*30]))
-        visualShapeId = pb.createVisualShape(shapeType=pb.GEOM_MESH,
-                                        fileName="pybullet_data/mussle_rope_part.obj",
-                                        visualFramePosition=[0,0,0],
-                                        visualFrameOrientation=vfo,
-                                        meshScale=meshScale)#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-        ret.append(pb.createMultiBody(baseMass=0,
-                          baseInertialFramePosition=[0, 0, 0],
-                          baseVisualShapeIndex=visualShapeId,
-                          basePosition=[x,y,i*1.2],
-                          useMaximalCoordinates=True))
-
-    return ret
-
-def getscene():
-    ret=[]
-    for j in range(2):
-        for i in range(5):
-            ret+=gen_rope(2+j*6,i*3)
-
-    meshScale = np.ones(3)*1
-    vfo=pb.getQuaternionFromEuler(np.deg2rad([-90, 0, 0]))
-    visualShapeId = pb.createVisualShape(shapeType=pb.GEOM_MESH,
-                                        fileName="pybullet_data/seabed1.obj",
-                                        visualFramePosition=[0,0,10],
-                                        visualFrameOrientation=vfo,
-                                        meshScale=meshScale)#set the center of mass frame (loadURDF sets base link frame) startPos/Ornp.resetBasePositionAndOrientation(boxId, startPos, startOrientation)
-    ret.append(pb.createMultiBody(baseMass=0,
-                          baseInertialFramePosition=[0, 0, 0],
-                          baseVisualShapeIndex=visualShapeId,
-                          basePosition=[0,0,0],
-                          useMaximalCoordinates=True))
- 
-    return ret
 
 
 def _get_next_state(curr_q,curr_u,control,dt,lamb):
