57c848dc4b1712bf74508749b73c2464a16fbd4f
ori <oga13@uclive.ac.nz>
2022-09-17 18:33:12 +0300

    gui click on image

--- ground_control/rovsg.py
+++ ground_control/rovsg.py
@@ -38,39 +38,90 @@ def img_to_tk(img,shrink=1):
         img = Image.fromarray(img[::shrink,::shrink])
     img = ImageTk.PhotoImage(img)
     return img
+def img_to_tk2(img,shrink=1):
+    if shrink==1:
+        img = Image.fromarray(img)
+    else:
+        img = Image.fromarray(img[::shrink,::shrink])
+    bio = io.BytesIO()
+    img.save(bio,format='PNG')
+    return bio.getvalue()
+
 
 
 def main():
     rovHandler = rovDataHandler(None)
     rovCommander = rovCommandHandler()
-    layout = [
-        [sg.Image(key="-MAIN-IMAGE-"),
-            sg.Image(key="-IMAGE-1-")],#,sg.Image(key="-IMAGE-2-")],
-        [sg.Canvas(key="-CANVAS-")],
+    im_size = (960,600) 
+    row1_layout = [[
+        sg.Graph(im_size, graph_bottom_left=(0, 0), graph_top_right=im_size ,key="-MAIN-IMAGE-",
+            change_submits=True,
+            enable_events=True,
+            ),
+        sg.Image(key="-IMAGE-1-"),#,sg.Image(key="-IMAGE-2-")],
+        ]]
 
-        [
-            sg.Text("Image File"),
-            sg.Input(size=(25, 1), key="-FILE-"),
-            sg.Button("Arm-Disarm"),
-        ],
+    cmd_column = [
+            [sg.Button('Arm-Disarm')],
+            [sg.Button('Depth-Hold'),sg.Button('D+'),sg.Button('D-'),sg.Input(key='Target-Depth',size=(4,1))],
+            [sg.Button('Att-hold')],
+            [sg.Button('X-hold'),sg.Button('X+'),sg.Button('X-'),sg.Input(key='Target-X',size=(4,1))],
+            [sg.Button('Y-hold'),sg.Button('Y+'),sg.Button('Y-'),sg.Input(key='Target-Y',size=(4,1))],
+            [sg.Button('Yaw+'),sg.Button('Yaw-')],
+            ]
+
+    yaw_source_options=['VNAV','DVL']
+    config_column = [
+            [sg.Text('Yaw Source:'),sg.Combo(yaw_source_options,key='YAW_SOURCE',default_value=yaw_source_options[0])]
+            ]
+
+    plot_options=['DEPTH','YAW','PITCH','ROLL','RANGE']
+    matplot_column = [
+        [sg.Text('Plot Type:'),sg.Combo(plot_options,key='-PLOT-TYPE-',default_value=plot_options[0])],
+        [ sg.Canvas(key="-CANVAS-", size=(500,500))]]
+    row2_layout = [[
+            #sg.Canvas(key="-CANVAS-", size=(500,500)),
+            sg.Column(matplot_column),
+            sg.VSeperator(),
+            sg.Column(cmd_column,vertical_alignment='top'),
+            sg.VSeperator(),
+            sg.Column(config_column,vertical_alignment='top'),
+    ]]
+
+    layout = [
+        [sg.Frame('',row1_layout)],
+        [sg.Frame('',row2_layout)],
     ]
+
+    
+
     window = sg.Window("ROV Viewer", 
             layout, finalize=True, 
             element_justification='left', 
             font='Helvetica 16',
             size=(1920,1280))
+    #window['-MAIN-IMAGE-'].bind('<Button-1>','')
     plotter = Plotter(window["-CANVAS-"].TKCanvas)
     
     while True:
         event, values = window.read(timeout=20) #10 mili timeout
+        #print('----------',event,values)
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
+
+        if event.startswith('-MAIN'):
+            print('----------',event.split(),values['-MAIN-IMAGE-'])
 
         frameId, rawImgs = rovHandler.getNewImages()
 
         if rawImgs is not None:
             #print('===',time.time(),rawImgs[0].shape)
-            window["-MAIN-IMAGE-"].update(data=img_to_tk(rawImgs[0]))
+            #print(rawImgs[0].shape)
+            window["-MAIN-IMAGE-"].draw_image(data=img_to_tk2(rawImgs[0]),location=(0,im_size[1]))#im_size[1]))
+            #window["-MAIN-IMAGE-"].draw_image(data=sg.EMOJI_BASE64_HAPPY_JOY, location=(0,20))
+   #window["-MAIN-IMAGE-"].update(data=img_to_tk(rawImgs[0]))
+            #window["-MAIN-IMAGE-"].draw_circle((75, 75), 25, fill_color='black', line_color='white')
+            #window['-MAIN-IMAGE-'].bind('Button-1','')
             #window["-IMAGE-1-"].update(data=img_to_tk(rawImgs[0]))
             window["-IMAGE-1-"].update(data=img_to_tk(rawImgs[1],1))
  
