54fdab4b25df903fe5512ba3ab9f2faeb6de49df
ori <o@g>
2022-09-30 02:34:05 +0300

    add reset to max lock

--- plugins/tracker/config_tracker.py
+++ plugins/tracker/config_tracker.py
@@ -1,4 +1,5 @@
 import numpy as np
+#track_offx=0
 track_offx=80
 #track_params = (60,60,60,60,track_offx,0)
 track_params = (150,150,150,150,track_offx,0)
@@ -6,8 +7,9 @@ stereo_corr_params = {'ws':(128,128),'sxl':250+50+200,'sxr':0,'ofx':100 ,'sxl2':
 fov=60.97
 diff_range_valid=1.0
 clear_freqs=5
-#max_diff_cols=50
-max_diff_cols=150
+max_diff_cols=50
+#max_diff_cols=150
+#max_diff_cols=1500
 ignore_extrema_type=False
 
 pixelwidthx = 2464//4
--- plugins/tracker/rope_detect.py
+++ plugins/tracker/rope_detect.py
@@ -45,3 +45,29 @@ def rope_detect(prev_col,extrema,start_row,nrows,im,clear_freqs=5,max_diff_cols=
     debug={}
     debug['ifft']=im2r
     return extrema,new_col,debug
+
+def rope_global_extrema(extrema,start_row,nrows,im,clear_freqs=5):
+    imt=im[start_row:start_row+nrows,:].sum(axis=0).flatten().astype(float)
+    #plt.plot(np.convolve(im1,np.concatenate((np.ones(8),-np.ones(8)))))
+    #conv_val=10
+    #im2=np.convolve(im1,np.ones(conv_val),'valid')
+    yy=np.fft.fft(imt)
+    #yy=np.fft.fftshift(yy) no need for shift high freqs in the middle
+    yy[clear_freqs:-clear_freqs]=0
+    #yy=np.fft.fftshift(yy)
+    im2r=np.fft.ifft(yy).real
+    im2r-=im2r.min()
+    im2r/=im2r.max()
+    #import pdb;pdb.set_trace()
+
+    
+    new_col=None
+    if extrema is 'max':
+        new_col = np.argmax(np.abs(im2r))
+        #import pdb;pdb.set_trace()
+    elif extrema == 'min':
+        new_col = np.argmin(np.abs(im2r))
+        #print('cossen extrema', extrema,new_col, maxima,minima)
+    debug={}
+    debug['ifft']=im2r
+    return extrema,new_col,debug
