e800f57ab371192b57ad69babcacefe478194edf
ori <o@g>
2022-09-30 12:35:14 +0300

    reducing ploting itterations

--- ground_control/rovsg.py
+++ ground_control/rovsg.py
@@ -190,8 +190,12 @@ def main():
     current_yaw_deg=0
     target_xy=[0,0]
     sg.cprint_set_output_destination(window, 'MESSEGES')
+
+    last_plot_pids=time.time()
+    last_plot_dvl =time.time()
     while True:
-        event, values = window.read(timeout=20) #10 mili timeout
+        cycle_tic=time.time()
+        event, values = window.read(timeout=2) #10 mili timeout
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
 
@@ -201,7 +205,7 @@ def main():
             y=y/image_shape[0]
             print('---click--',x,y)
             rovCommander.lock(x,y)
-
+        tic1=time.time()
         frameId, rawImgs = rovHandler.getNewImages()
         if 0 and  scale_screen and rawImgs is not None:
             sy,sx=rawImgs[0].shape[:2]
@@ -210,6 +214,7 @@ def main():
             #rawImgs = [cv2.resize(im,(sx,sy)) for im in rawImgs[:2]]
             print('>>>>',sx,sy)
 
+        tic2=time.time()
         if rawImgs is not None:
             image_shape=rawImgs[0].shape
             #print('===',time.time(),rawImgs[0].shape)
@@ -222,6 +227,7 @@ def main():
             #last_im=window["-MAIN-IMAGE-"].draw_image(data=img_to_tk3(rawImgs[0]),location=(0,0))#im_size[1]))
             last_im=draw_image(window["-MAIN-IMAGE-"],img_to_tk(rawImgs[0],h_hsv=values['HSV_H']))#im_size[1]))
             window["-IMAGE-1-"].update(data=img_to_tk(rawImgs[1],1))
+            #print(f'=== tk images took {(time.time()-tic1)*1000:.1f} msec, grab  {(time.time()-tic2)*1000:.1f} msec')
  
         if event == "Arm-Disarm":
             rovCommander.armdisarm()
@@ -332,21 +338,34 @@ def main():
             target_xy[i]=pb.get_last('T')
             if target_xy[i] is None:
                 target_xy[i]=0
-        for p_type,pb in [\
-                ('YAW',zmq_topics.topic_att_hold_yaw_pid),
-                ('PITCH',zmq_topics.topic_att_hold_pitch_pid),
-                ('ROLL',zmq_topics.topic_att_hold_roll_pid)]:
-            if values['-PLOT-TYPE-']==p_type:
-                plotter.update_pid(rovHandler.plot_buffers[pb])
+        tic=time.time()
+        if time.time()-last_plot_pids>0.3:
+            last_plot_pids=time.time()
+            for p_type,pb in [\
+                    ('YAW',zmq_topics.topic_att_hold_yaw_pid),
+                    ('PITCH',zmq_topics.topic_att_hold_pitch_pid),
+                    ('ROLL',zmq_topics.topic_att_hold_roll_pid)]:
+                if values['-PLOT-TYPE-']==p_type:
+                    plotter.update_pid(rovHandler.plot_buffers[pb])
 
         rovHandler.next()
+
         rov_telem=rovHandler.getTelemtry()
-        if 'dvl_deadrecon' in rov_telem:
+        if 'dvl_deadrecon' in rov_telem :
             trace_plotter.update_dvl_data(rov_telem['dvl_deadrecon'],target_pos=target_xy[::-1],yaw_deg=current_yaw_deg)
+        if time.time()-last_plot_dvl>0.3:
+            last_plot_dvl=time.time()
+            trace_plotter.redraw()
+
         if zmq_topics.topic_imu in rov_telem:
             current_yaw_deg = rov_telem[zmq_topics.topic_imu]['yaw']
+        if 1:
+            print(f'=== took {(time.time()-tic)*1000:.1f} msec')
 
         cnt+=1
+        if 0:
+            print(f'=== tk images took {(time.time()-cycle_tic)*1000:.1f} msec')
+
     window.close()
 if __name__ == "__main__":
     main()
