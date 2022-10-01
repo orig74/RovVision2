a898e286ac77483df550045372bd45a6c2de40cf
ori <o@g>
2022-10-01 04:26:31 +0300

    add target yaw

--- ground_control/rov_data_handler.py
+++ ground_control/rov_data_handler.py
@@ -279,6 +279,9 @@ class rovDataHandler(object):
     def get_target_depth(self):
         return self.telemtry.get(zmq_topics.topic_depth_hold_pid,{}).get('T',0)
 
+    def get_target_yaw(self):
+        return self.telemtry.get(zmq_topics.topic_att_hold_yaw_pid,{}).get('T',0)
+
     def toggle_recording(self):
         if not self.record_state:
             self.record_state=datetime.now().strftime('%y%m%d-%H%M%S') 
