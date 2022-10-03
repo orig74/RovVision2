a11f24aa2e1738ee72b5690725b55bb983fc1ede
uav <uav@new>
2022-10-04 04:54:36 +1300

    update flat port

--- sim/pybullet_bridge.py
+++ sim/pybullet_bridge.py
@@ -160,6 +160,7 @@ def main():
     curr_q = np.zeros(6)
     curr_u = np.zeros(6)
     current_command = np.zeros(8)
+    fov=42
 
     if render==pb.GUI:
         boxId = getrov()
@@ -195,7 +196,7 @@ def main():
 
             #VM = pb.computeViewMatrixFromYawPitchRoll(pos,0.1,*pb.getEulerFromQuaternion(quat),2)
             #print('===',len(VML))
-            PM = pb.computeProjectionMatrixFOV(fov=60.0,aspect=1.0,nearVal=0.1,farVal=100)
+            PM = pb.computeProjectionMatrixFOV(fov=fov,aspect=1.0,nearVal=0.1,farVal=100)
             w = int(config.cam_res_rgbx*resize_fact)
             h = int(config.cam_res_rgby*resize_fact)
             width, height, rgbImg, depthImg, segImg = pb.getCameraImage(
@@ -218,7 +219,7 @@ def main():
             if not mono:
                 #CM = np.array(pb.computeViewMatrix((0,bl,0),(1,bl,0),(0,0,-1))).reshape((4,4),order='F')
                 VMR=CM @ translateM(0,-bl/2,0) @ MdsymI #left camera 0.2 for left 
-                PM = pb.computeProjectionMatrixFOV(fov=60.0,aspect=1.0,nearVal=0.1,farVal=1000)
+                PM = pb.computeProjectionMatrixFOV(fov=fov,aspect=1.0,nearVal=0.1,farVal=1000)
                 width, height, rgbImg, depthImg, segImg = pb.getCameraImage(
                     width=w, 
                     height=h,
