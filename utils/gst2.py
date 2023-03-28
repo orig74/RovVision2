ddbcb74d6aca49cea06781c0da761181e52b458b
o <o@g>
2023-03-28 18:20:38 +0300

    new gst version

---
+++ utils/gst2.py
@@ -0,0 +1,67 @@
+from subprocess import Popen,PIPE
+import sys,time,select,os
+import numpy as np
+import config
+import image_enc_dec
+import cv2
+#
+class Writer(object):
+    def __init__(self,port,sx,sy):
+        cmd="gst-launch-1.0 {{}}! x264enc threads=1 speed-preset={} tune=zerolatency  bitrate={} key-int-max=50 ! tcpserversink port={{}}".format(config.gst_speed_preset, config.gst_bitrate)
+        gstsrc = 'fdsrc ! videoparse width={} height={} format=15 ! videoconvert ! video/x-raw, format=I420'.format(sx,sy) #! autovideosink'
+
+        gcmd = cmd.format(gstsrc,port)
+        self.p = Popen(gcmd, shell=True, bufsize=0,stdin=PIPE, stdout=sys.stdout, close_fds=False)
+        print('gcmd = ',gcmd)
+        self.cnt=0
+
+    def write(self,im):
+        if len(select.select([],[self.p.stdin],[],0.005)[1])>0:
+            self.p.stdin.write(im.tostring())
+            self.cnt+=1
+
+class Reader(object):
+    def __init__(self,name,port,sx,sy):
+        if 1: #h264
+            cmd='gst-launch-1.0 tcpclientsrc port={} ! identity sync=true  ! tee name=t ! queue ! filesink location=fifo_264_{}  sync=false  t. ! queue !'+\
+            ' h264parse ! decodebin ! videoconvert ! video/x-raw,height={},width={},format=RGB ! filesink location=fifo_raw_{}  sync=false'
+        if 0:
+            cmd='gst-launch-1.0 -q udpsrc port={} ! application/x-rtp,encoding-name=JPEG,payload=26 ! rtpjpegdepay ! jpegdec ! videoconvert ! video/x-raw,height={},width={},format=RGB ! fdsink'
+
+        os.system('rm fifo_*')
+        fname_264='fifo_264_'+name
+        os.mkfifo(fname_264)
+        r = os.open(fname_264,os.O_RDONLY | os.O_NONBLOCK)
+        fname_raw='fifo_raw_'+name
+        os.mkfifo(fname_raw)
+        r1 = os.open(fname_raw,os.O_RDONLY | os.O_NONBLOCK)
+        gcmd = cmd.format(port,name,sy,sx,name)
+        print(gcmd)
+        self.pipe_264=r
+        self.pipe=r1
+        Popen(gcmd, shell=True, bufsize=0)
+        self.sxsy=(sx,sy)
+        self.save_fd=None
+
+    def set_save_fd(self,fd):
+        self.save_fd=fd
+
+    def get_imgs(self):
+        sx,sy=self.sxsy
+        image=None
+        while len(select.select([self.pipe],[],[],0.005)[0])>0:
+            data=b''
+            while len(data)<sx*sy*3:
+                try:
+                    data+=os.read(self.pipe,sx*sy*3-len(data))
+                except BlockingIOError:
+                    time.sleep(0.001)
+            try:
+                image = np.fromstring(data,'uint8').reshape([sy,sx,3])[:,:,::-1].copy()
+            except:
+                image = np.zeros((sy, sx, 3), dtype=np.uint8)
+        if len(select.select([self.pipe_264],[],[],0.005)[0])>0:
+            data=os.read(self.pipe_264,1*1000*1000)
+            if self.save_fd is not None:
+                self.save_fd.write(data)
+        return image
