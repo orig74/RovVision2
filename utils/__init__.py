4061549ac4219e30abb27c2ca6de5af8e5a615a5
uav <uav@new>
2023-04-22 18:15:59 +1200

    bad param

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,7 @@
 import cv2
 def im16to8_22(im):
-    return cv2.applyColorMap(cv2.convertScaleAbs(im[::2,::2].copy(), alpha=0.03), cv2.COLORMAP_JET)
-
+    #return cv2.applyColorMap(cv2.convertScaleAbs(im[::2,::2].copy(), alpha=0.03), cv2.COLORMAP_JET)
+    imret = cv2.applyColorMap(cv2.convertScaleAbs(im[::2,::2].copy(), alpha=0.46), cv2.COLORMAP_JET)[:,:,::-1]
+    #imret[im==0]=(0,0,0)
+    return imret
 
