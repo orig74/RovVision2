88888e3bc6cec32e7dafaf750ff2acdc9027c61a
uav <uav@new>
2023-04-25 17:31:52 +1200

    tracker crop

--- plugins/tracker_main.py
+++ plugins/tracker_main.py
@@ -16,16 +16,20 @@ import zmq_topics
 import config
 from tracker.of import OF
 
+cam_main_inv=np.linalg.inv(np.array(config.cam_main_int))
 printer_source = zmq_wrapper.push_source(zmq_topics.printer_sink_port)
 
 def printer(txt,c=None):
     print('printing:',txt)
     printer_source.send_pyobj({'txt':txt,'c':c})
 
-def rope_detect_depth(depth_img,start_row=150,nrows=100):
+def rope_detect_depth(depth_img,scale_to_mm,start_row=150,nrows=50):
     #np.save('/tmp/imgd.npy',depth_img)
     marg=100
-    imt=depth_img[start_row:start_row+nrows,:].sum(axis=0).flatten()/nrows
+    sliceimg=depth_img[start_row:start_row+nrows,:]
+    scaled_d = sliceimg*scale_to_mm*config.water_scale
+    scaled_d[scaled_d<1]=10000 #10 meters
+    imt=scaled_d.sum(axis=0).flatten()/nrows
     #prioritizing center
     r=600
     imtp=imt+np.abs(np.linspace(-r,r,len(imt)))
@@ -34,13 +38,15 @@ def rope_detect_depth(depth_img,start_row=150,nrows=100):
     col=np.argmin(imtp[marg:-marg])+marg
     return imt[col],col,imtp
 
+crop_ltrb=(200,100,200,2)
+
 async def recv_and_process():
     keep_running=True
     subs_socks=[]
     subs_socks.append(zmq_wrapper.subscribe([zmq_topics.topic_remote_cmd],zmq_topics.topic_remote_cmd_port))
 
     vid_topics=[zmq_topics.topic_main_cam,zmq_topics.topic_main_cam_depth,zmq_topics.topic_main_cam_ts]
-    subs_socks.append(zmq_wrapper.subscribe(vid_topics+[zmq_topics.topic_main_cam_ts], zmq_topics.topic_main_cam_port)) #for sync perposes
+    subs_socks.append(zmq_wrapper.subscribe(vid_topics, zmq_topics.topic_main_cam_port)) #for sync perposes
 
     ### plugin outputs
     sock_pub=zmq_wrapper.publisher(zmq_topics.topic_main_tracker_port)
@@ -48,12 +54,13 @@ async def recv_and_process():
     of=OF()
     last_depth16=None
     scale_to_mm=0
+    main_image=None
     while keep_running:
         socks=zmq.select(subs_socks,[],[],0.005)[0]
         for sock in socks:
             ret=sock.recv_multipart()
             data = pickle.loads(ret[1]) if ret[0] not in vid_topics else None
-            if ret[0]==zmq_topics.topic_remote_cmd:
+            if main_image is not None and ret[0]==zmq_topics.topic_remote_cmd:
                 if data['cmd']=='main_track':
                     print('got click',data['click_pt'])
                     pt=data['click_pt']
@@ -63,8 +70,10 @@ async def recv_and_process():
                         
                     if im_gray is not None:
                         x,y=pt
-                        x=int(x*im_gray.shape[1])
-                        y=int(y*im_gray.shape[0])
+                        x=int(x*main_image.shape[1])-crop_ltrb[0]
+                        y=int(y*main_image.shape[0])-crop_ltrb[1]
+                        #x=int(x*im_gray.shape[1])
+                        #y=int(y*im_gray.shape[0])
                         of.set(im_gray,(x,y))
                         print('setting track',x,y)
  
@@ -73,13 +82,19 @@ async def recv_and_process():
 
             if ret[0]==zmq_topics.topic_main_cam:
                 frame_main_cnt,shape = pickle.loads(ret[1])
-                main_image=np.frombuffer(ret[2],'uint8').reshape(shape).copy()
-                im_gray=cv2.cvtColor(main_image, cv2.COLOR_BGR2GRAY)
+                main_image=np.frombuffer(ret[2],'uint8').reshape(shape)#.copy()
+                rows,cols=main_image.shape[:2]
+                #im_gray=cv2.cvtColor(main_image, cv2.COLOR_BGR2GRAY)
+                l,t,r,b=crop_ltrb
+                im_gray=main_image[t:-b,l:-r,1].copy()
+                #im_gray=main_image[l:-r,t:-b,1].copy()
+                
                 _ret=of.track(im_gray)
-                rows,cols=im_gray.shape
+                #_ret=None
 
                 xw,yw,d=None,None,None
                 if _ret is not None:
+                    _ret=(_ret[0]+l,_ret[1]+t)
                     if last_depth16 is not None:
                         #print('ret===',_ret)
                         try:
@@ -90,7 +105,7 @@ async def recv_and_process():
 
                         if config.valid_range_mm[0]<d<config.valid_range_mm[1]:
                             xw,yw,s=(np.linalg.inv(np.array(config.cam_main_int)) @ np.array([[_ret[0]*d,_ret[1]*d,d]]).T).flatten()
-                            print('===+===',xw,yw,s,_ret)
+                            #print('===+===',xw,yw,s,_ret)
                         else:
                             d=None
                     _ret=[_ret[0]/cols,_ret[1]/rows]
@@ -102,14 +117,14 @@ async def recv_and_process():
             if ret[0]==zmq_topics.topic_main_cam_depth:
                 _,scale_to_mm,shape = pickle.loads(ret[1])
                 last_depth16=np.frombuffer(ret[2],'uint16').reshape(shape)
-                scaled_d = last_depth16.astype('float')*scale_to_mm*config.water_scale
-                scaled_d[scaled_d<1]=10000 #10 meters
-                #import ipdb;ipdb.set_trace()
-                d,col,_=rope_detect_depth(scaled_d)
-                xw,yw,s=(np.linalg.inv(np.array(config.cam_main_int)) @ np.array([[col*d,0*d,d]]).T).flatten()
-                res={'rope_col':col/last_depth16.shape[1],'range':s/1000,'dy':xw/1000,'valid':True}
-                print('returning rope position: ',res)
-                sock_pub.send_multipart([zmq_topics.topic_tracker,pickle.dumps(res)])
+                d,col,_=rope_detect_depth(last_depth16,scale_to_mm)
+                xw,yw,s=(cam_main_inv @ np.array([[col*d,0*d,d]]).T).flatten()
+                if 1:
+                    #import ipdb;ipdb.set_trace()
+                    #d,col,_=rope_detect_depth(scaled_d)
+                    res={'rope_col':col/last_depth16.shape[1],'range':s/1000,'dy':xw/1000,'valid':True}
+                    #print('returning rope position: ',res)
+                    sock_pub.send_multipart([zmq_topics.topic_tracker,pickle.dumps(res)])
 
                 
 
