7de04ec633f31eac16b25360c2e904c9ccc64b08
uav <uav@new>
2023-07-27 15:27:42 +1200

    tested data scripts

--- scripts/rsync_GC2UC.sh
+++ scripts/rsync_GC2UC.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 read -p "Enter UC username: " UCusername
-rsync -avz --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r -P --remove-source-files /home/uav/data/* $UCusername@cssecs7.canterbury.ac.nz:/csse/research/CVlab/bluerov_data
+rsync -avz --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r -P --remove-source-files ~/data/* $UCusername@cssecs7.canterbury.ac.nz:/csse/research/CVlab/bluerov_data
 echo "Deleting empty source directories..."
-find /home/uav/data/* -type d -empty -delete
+find ~/data/* -type d -empty -delete
 read -p "Press ENTER to exit..." _
 # /media/uav/Expansion/data/*
