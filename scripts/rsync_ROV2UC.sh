118dcddda0fe386eebdd4f2aab1402261646caf8
Tim <timrensen@yahoo.com>
2023-09-26 14:19:07 +1300

    gave other group members write perm.

--- scripts/rsync_ROV2UC.sh
+++ scripts/rsync_ROV2UC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 read -p "Enter UC username: " UCusername
-rsync -avz --chmod=Du=rwx,Dg=rx,Do=rx,Fu=rw,Fg=r,Fo=r -P --remove-source-files ~/../../media/data/* $UCusername@cssecs7.canterbury.ac.nz:/csse/research/CVlab/bluerov_data
+rsync -avz --chmod=Du=rw,Dg=rw,Do=r,Fu=rw,Fg=rw,Fo=r -P --remove-source-files ~/../../media/data/* $UCusername@cssecs7.canterbury.ac.nz:/csse/research/CVlab/bluerov_data
 echo "Deleting empty source directories..."
 find ~/../../media/data/* -type d -empty -delete
 read -p "Press ENTER to exit..." _
